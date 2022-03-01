9102754132e5e673de23fe820e64fb0c4c6b3be3
Folalu Tomiwa <tomiczilla@gmail.com>
2022-03-01 23:18:34 +0100

    the layout editor homework

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mzero = 0;
     private int mCount = 0;
     private TextView mShowCount;
+    private Button countBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        countBtn = (Button) findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -28,5 +32,13 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        if(mCount %2 == 0){
+            countBtn.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
+
+    public void zeroDown(View view) {
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mzero));
     }
 }
